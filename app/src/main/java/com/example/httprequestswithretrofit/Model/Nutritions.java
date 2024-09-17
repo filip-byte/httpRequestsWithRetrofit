4d33c1910c48e7e2ce9bee4ad099567e2266691f
filip-byte <ohboytoby@gmail.com>
2024-09-17 12:29:19 +0100

    feat: add Nutritions model class

---
+++ app/src/main/java/com/example/httprequestswithretrofit/Model/Nutritions.java
@@ -0,0 +1,61 @@
+package com.example.httprequestswithretrofit.Model;
+
+public class Nutritions {
+
+    private double calories;
+    private double fat;
+    private double sugar;
+    private double carbohydrates;
+    private double protein;
+
+    public Nutritions(double calories, double fat, double sugar, double carbohydrates, double protein) {
+        this.calories = calories;
+        this.fat = fat;
+        this.sugar = sugar;
+        this.carbohydrates = carbohydrates;
+        this.protein = protein;
+    }
+
+    public Nutritions() {
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getSugar() {
+        return sugar;
+    }
+
+    public void setSugar(double sugar) {
+        this.sugar = sugar;
+    }
+
+    public double getCarbohydrates() {
+        return carbohydrates;
+    }
+
+    public void setCarbohydrates(double carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+}
