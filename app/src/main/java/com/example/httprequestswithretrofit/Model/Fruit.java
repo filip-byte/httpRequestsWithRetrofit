0278bb65fc36b24281c592826860fa2828d9be7d
filip-byte <ohboytoby@gmail.com>
2024-09-17 12:28:53 +0100

    feat: add Fruit model class

---
+++ app/src/main/java/com/example/httprequestswithretrofit/Model/Fruit.java
@@ -0,0 +1,71 @@
+package com.example.httprequestswithretrofit.Model;
+
+public class Fruit {
+
+    private String name;
+    private int id;
+    private String family;
+    private String order;
+    private String genus;
+    private Nutritions nutritions;
+
+    public Fruit(String name, int id, String family, String order, String genus, Nutritions nutritions) {
+        this.name = name;
+        this.id = id;
+        this.family = family;
+        this.order = order;
+        this.genus = genus;
+        this.nutritions = nutritions;
+    }
+
+    public Fruit() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public String getGenus() {
+        return genus;
+    }
+
+    public void setGenus(String genus) {
+        this.genus = genus;
+    }
+
+    public Nutritions getNutritions() {
+        return nutritions;
+    }
+
+    public void setNutritions(Nutritions nutritions) {
+        this.nutritions = nutritions;
+    }
+}
